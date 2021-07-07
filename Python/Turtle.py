dac663e94f87f89284a8662dd3ba111082d8a053
YamCham0 <yajochakha@gmail.com>
2021-07-07 03:06:19 -0500

    looping

--- Python/Turtle.py
+++ Python/Turtle.py
@@ -3,7 +3,7 @@ import turtle
 
 
 jack = turtle.Turtle()
-jack.speed(0)
+jack.speed(4)
 jack.width(3)
 jack.color("red")
 jack.penup()
@@ -12,10 +12,12 @@ jack.pendown()
 
 def draw_polygon(length, sides, color):
     jack.color(color)
-    for side in range(4):
+    for side in range(8):
         jack.forward(length)
         jack.right(sides)
         
 draw_polygon(100, 90, "yellow")
 draw_polygon(200, 72, "blue")
 draw_polygon(300, 120, "green")
+draw_polygon(200, 60, "red")
+draw_polygon(100, 45, "purple")
---
+++ Python/abracadabra.py
@@ -0,0 +1,5 @@
+s = "abracadabra"
+index = len(s)
+while index > 0:
+    index -= 1
+    print(s[:index])
